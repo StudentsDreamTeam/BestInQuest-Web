995ba02a870339b1c50172922632df6223d9a2e1
Darlitch <102824539+Darlitch@users.noreply.github.com>
2025-05-05 21:23:02 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     react(),
     svgr()
   ],
-  base: process.env.GITHUB_REF === 'refs/heads/main' ? '/' : '/dev/',
+  base: process.env.GITHUB_REF === 'refs/heads/develop' ? '/dev/' : '/',
   build: {
     outDir: 'dist',          // явно указываем выходную папку
     assetsDir: 'assets',
