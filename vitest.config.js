ad722f023721c8d68ffa05d7ccbdc994e162c986
Darlitch <102824539+Darlitch@users.noreply.github.com>
2025-05-30 03:54:15 +0700

    Update vitest.config.js

--- vitest.config.js
+++ vitest.config.js
@@ -2,19 +2,10 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    environment: 'jsdom',
-    globals: true,
-    setupFiles: ['./src/setupTests.js'],
     coverage: {
-      provider: 'c8',               // Используем c8 для покрытия
-      reporter: ['text', 'json', 'lcov'], // lcov нужен для SonarCloud
-      reportsDirectory: './coverage', // Папка для отчётов
-      include: ['src/**/*.{js,jsx,ts,tsx}'], // Что проверяем
-      exclude: [                     // Что игнорируем
-        '**/__mocks__/**',
-        '**/*.d.ts',
-        'src/main.{jsx,tsx}',
-      ],
+      provider: 'v8', // или 'istanbul' (если 'v8' не работает)
+      reporter: ['text', 'lcov'], // 'lcov' для Sonar
+      reportsDirectory: './coverage', // папка с отчетами
     },
   },
 });
